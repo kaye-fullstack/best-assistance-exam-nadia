287e52b898d380728dc7e67bc1f521962b5756d2
nadia-fullstack <nadia@fullstack.ph>
2021-03-02 16:27:07 +0800

    initial commit

---
+++ src/scripts/modules/theme-module.js
@@ -0,0 +1,67 @@
+AppName.Modules.ThemeModule = (function () {
+  //Dependencies
+  var core = AppName.Core;
+
+  //////////////////////
+  // Private Methods //
+  ////////////////////
+  const _privateMethod = () => {
+    // private stuff
+
+    var swiperLogical = new Swiper('.swiper-logical .swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  };
+
+  const _swiperReading = () => {
+    var swiperRead = new Swiper('.swiper-reading .swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+      pagination: {
+        el: '.swiper-pagination-reading',
+        clickable: true,
+      },
+    });
+  };
+
+  const _filterBy = () => {
+    $('select').change(function(){
+      var value = $(this).val();
+      if(value == 'filterBy'){
+        $('#news', '#health', '#success', '#case').show();
+      }
+      $('#news').hide();
+      if(value == 'news'){
+          $('#news').show();
+      } 
+      $('#health').hide();
+      if(value == 'health'){
+        $('#health').show();
+      } 
+      $('#success').hide();
+      if(value == 'success'){
+        $('#success').show();
+      } 
+      $('#case').hide();
+      if(value == 'case'){
+      $('#case').show();
+      } 
+    })
+  };
+
+  /////////////////////
+  // Public Methods //
+  ///////////////////
+  const init = function () {
+    _privateMethod();
+    _swiperReading();
+    _filterBy();
+  };
+
+  return {
+    init: init,
+  };
+})();
